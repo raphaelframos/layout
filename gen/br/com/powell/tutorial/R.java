b9d43677bfc74d5e12148b3d1bf58795ddd61ec8
raphaelframos <raphaelframos@gmail.com>
2012-12-30 12:08:08 -0200

    Criando lista com opcoes de layout

--- gen/br/com/powell/tutorial/R.java
+++ gen/br/com/powell/tutorial/R.java
@@ -8,6 +8,9 @@
 package br.com.powell.tutorial;
 
 public final class R {
+    public static final class array {
+        public static final int layout=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,14 +18,14 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int listView1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int listViewLayout=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -31,6 +34,6 @@ public final class R {
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- src/br/com/powell/tutorial/MainActivity.java
+++ src/br/com/powell/tutorial/MainActivity.java
@@ -2,12 +2,22 @@ package br.com.powell.tutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	private ListView listaOpcoesLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        listaOpcoesLayout = (ListView) findViewById(R.id.listViewLayout);
+ 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+        		android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.layout));
+        listaOpcoesLayout.setAdapter(adapter);
     }
 }
